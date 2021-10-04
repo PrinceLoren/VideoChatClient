367ad40612e42b7d2a1434f93a447d9903dca970
PrincexGod <cvb.amaho@gmail.com>
2021-10-04 14:36:27 +0200

    answerCall

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import {Typography, AppBar} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+import VideoPlayer from "./components/VideoPlayer";
+import Option from "./components/Option";
+import Notification from "./components/Notification";
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderRadius: 10,
+        margin: '30px 100px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '600px',
+        border: '2px solid #13e836',
+        background: '#a113e8',
+
+        [theme.breakpoints.down('xs')]: {
+            width: '90%',
+        },
+    },
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+    },
+}))
+
 
 const App = () => {
+    const classes = useStyles()
     return (
-        <div>
-            <AppBar position="static" color="inherit">
-                <Typography variant="12" align="center">Video Chat</Typography>
+        <div className={classes.wrapper}>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography variant="h2" align="center">Video Chat</Typography>
             </AppBar>
 
-        {/*   VideoPlayer */}
-        {/*    Option Component -> Notification*/}
+        <VideoPlayer />
+        <Option>
+            <Notification />
+        </Option>
         </div>
     );
 };
