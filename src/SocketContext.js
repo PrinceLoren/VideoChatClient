cf21d891d980274ba97a32dadce7a10ed43fc171
PrincexGod <cvb.amaho@gmail.com>
2021-10-04 14:46:04 +0200

    leaveCall

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -73,6 +73,33 @@ const ContextProvider = ({children}) => {
         connectionRef.current = peer
     }
 
-    const leaveCall = () => {}
+    const leaveCall = () => {
+        setCallEnded(true)
+
+        connectionRef.current.destroy()
+
+        window.location.reload()
+    }
+
+    return (
+        <SocketContext.Provider value={{
+            call,
+            callAccepted,
+            myVideo,
+            userVideo,
+            stream,
+            name,
+            setName,
+            callEnded,
+            me,
+            callUser,
+            leaveCall,
+            answerCall
+        }}>
+            {children}
+        </SocketContext.Provider>
+    )
 
 }
+
+export {ContextProvider, SocketContext}
